60e696df2505a1e0b8316fd3af1d87036e2c62aa
acontell <arcadio.contell@gmail.com>
2018-06-23 20:07:45 +0200

    Some improvements

--- vending_machine/display_test.py
+++ vending_machine/display_test.py
@@ -6,26 +6,23 @@ from tests_fixture import TestsFixture
 
 class DisplayTests(unittest.TestCase):
 
-    def setUp(self):
-        self.display = TestsFixture.DISPLAY
-
     def test_should_display_amount(self):
-        self.assertEqual(self.display.display_amount(5), TestsFixture.AMOUNT_TPL % 5)
+        self.assertEqual(TestsFixture.DISPLAY.display_amount(5), TestsFixture.AMOUNT_TPL % 5)
 
     def test_should_display_insert_coin_msg_when_amount_is_zero(self):
-        self.assertEqual(self.display.display_amount(0), TestsFixture.NO_COINS_MSG)
+        self.assertEqual(TestsFixture.DISPLAY.display_amount(0), TestsFixture.NO_COINS_MSG)
 
     def test_should_display_return_coins_msg(self):
-        self.assertEqual(self.display.display_return({"PENNY": 2}), "PENNY:\t2")
+        self.assertEqual(TestsFixture.DISPLAY.display_return({"PENNY": 2}), "PENNY:\t2")
 
     def test_should_display_empty_string_when_return_coins_map_is_empty(self):
-        self.assertEqual(self.display.display_return({}), "")
+        self.assertEqual(TestsFixture.DISPLAY.display_return({}), "")
 
     def test_should_display_price_msg(self):
-        self.assertEqual(self.display.display_price(), TestsFixture.PRICE_MSG)
+        self.assertEqual(TestsFixture.DISPLAY.display_price(), TestsFixture.PRICE_MSG)
 
     def test_should_display_thanks_msg(self):
-        self.assertEqual(self.display.display_thanks(), TestsFixture.THANKS_MSG)
+        self.assertEqual(TestsFixture.DISPLAY.display_thanks(), TestsFixture.THANKS_MSG)
 
         if __name__ == '__main__':
             unittest.main()
--- vending_machine/measure_test.py
+++ vending_machine/measure_test.py
@@ -2,25 +2,25 @@
 
 import unittest
 from measure import Measure
+from tests_fixture import TestsFixture
 
 
 class MeasureTests(unittest.TestCase):
-    __MEASURE = Measure(1, 1)
 
     def test_should_return_equals_when_same_measure(self):
-        self.assertTrue(self.__MEASURE.__eq__(Measure(1, 1)))
+        self.assertTrue(TestsFixture.MEASURE_1.__eq__(Measure(1, 1)))
 
     def test_should_return_not_equal_when_different_weight(self):
-        self.assertFalse(self.__MEASURE.__eq__(Measure(2, 1)))
+        self.assertFalse(TestsFixture.MEASURE_1.__eq__(Measure(2, 1)))
 
     def test_should_return_not_equal_when_different_size(self):
-        self.assertFalse(self.__MEASURE.__eq__(Measure(1, 2)))
+        self.assertFalse(TestsFixture.MEASURE_1.__eq__(Measure(1, 2)))
 
     def test_should_return_not_equal_when_different_type_of_object(self):
-        self.assertFalse(self.__MEASURE.__eq__(1))
+        self.assertFalse(TestsFixture.MEASURE_1.__eq__(1))
 
     def test_should_return_same_hash_for_same_object(self):
-        self.assertEqual(self.__MEASURE.__hash__(), Measure(1, 1).__hash__())
+        self.assertEqual(TestsFixture.MEASURE_1.__hash__(), Measure(1, 1).__hash__())
 
         if __name__ == '__main__':
             unittest.main()
