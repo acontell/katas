909da77f32647265e316bd2fb6e67ff70f06e1bb
acontell <arcadio.contell@gmail.com>
2020-01-18 16:07:50 +0100

    - Some test refactoring: --- Improved test names

--- weather_melia/src/test/java/training/weather/services/WeatherForecastTest.java
+++ weather_melia/src/test/java/training/weather/services/WeatherForecastTest.java
@@ -22,7 +22,6 @@ import static training.weather.WeatherForecastFixture.DATE_IN_RANGE_NOT_FOUND_TO
 import static training.weather.WeatherForecastFixture.DATE_IN_RANGE_TO_LOCAL_DATE;
 import static training.weather.WeatherForecastFixture.DATE_NOT_IN_RANGE;
 import static training.weather.WeatherForecastFixture.DATE_NOT_IN_RANGE_TO_LOCAL_DATE;
-import static training.weather.WeatherForecastFixture.PREDICTION_1;
 import static training.weather.WeatherForecastFixture.PREDICTION_FOR_DATE_IN_RANGE_1;
 import static training.weather.services.WeatherForecast.NO_WEATHER_STATE;
 import static training.weather.services.WeatherForecast.OFFSET_IN_DAYS;
@@ -39,40 +38,50 @@ public class WeatherForecastTest {
     private WeatherForecast weatherForecast;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         this.weatherForecast = new WeatherForecast(this.cityService, this.consolidatedWeatherService, this.clock);
-        given(this.cityService.getCity(CITY_NAME)).willReturn(CITY);
-        given(this.consolidatedWeatherService.getConsolidatedWeather(CITY)).willReturn(CONSOLIDATED_WEATHER);
-        given(this.clock.getLocalDateOrNow(DATE_IN_RANGE)).willReturn(DATE_IN_RANGE_TO_LOCAL_DATE);
-        given(this.clock.getLocalDateOrNow(null)).willReturn(DATE_IN_RANGE_TO_LOCAL_DATE);
-        doCallRealMethod().when(this.clock).isSameDate(any(), any());
     }
 
     @Test
     public void should_create_now_when_date_is_null() throws IOException {
-        given(this.clock.isDateBetweenRange(DATE_IN_RANGE_TO_LOCAL_DATE, OFFSET_IN_DAYS)).willReturn(true);
+        given_has_prediction();
+        given_clock_works_with_null();
         final String actual = this.weatherForecast.getCityWeather(CITY_NAME, null);
         assertEquals(PREDICTION_FOR_DATE_IN_RANGE_1, actual);
+    }
+
+    private void given_has_prediction() throws IOException {
+        given(this.cityService.getCity(CITY_NAME)).willReturn(CITY);
+        given(this.consolidatedWeatherService.getConsolidatedWeather(CITY)).willReturn(CONSOLIDATED_WEATHER);
+        given(this.clock.getLocalDateOrNow(DATE_IN_RANGE)).willReturn(DATE_IN_RANGE_TO_LOCAL_DATE);
+        given(this.clock.isDateBetweenRange(DATE_IN_RANGE_TO_LOCAL_DATE, OFFSET_IN_DAYS)).willReturn(true);
+        doCallRealMethod().when(this.clock).isSameDate(any(), any());
+    }
+
+    private void given_clock_works_with_null() {
+        given(this.clock.getLocalDateOrNow(null)).willReturn(DATE_IN_RANGE_TO_LOCAL_DATE);
+        given(this.clock.isDateBetweenRange(DATE_IN_RANGE_TO_LOCAL_DATE, OFFSET_IN_DAYS)).willReturn(true);
     }
 
     @Test
     public void should_return_no_results_when_date_is_not_in_range() throws IOException {
         given(this.clock.isDateBetweenRange(DATE_NOT_IN_RANGE_TO_LOCAL_DATE, OFFSET_IN_DAYS)).willReturn(false);
-        final String forecastToday = weatherForecast.getCityWeather(CITY_NAME, DATE_NOT_IN_RANGE);
-        assertEquals(forecastToday, NO_WEATHER_STATE);
+        final String actual = this.weatherForecast.getCityWeather(CITY_NAME, DATE_NOT_IN_RANGE);
+        assertEquals(NO_WEATHER_STATE, actual);
     }
 
     @Test
     public void should_return_prediction_when_date_is_in_range() throws IOException {
-        given(this.clock.isDateBetweenRange(DATE_IN_RANGE_TO_LOCAL_DATE, OFFSET_IN_DAYS)).willReturn(true);
+        given_has_prediction();
         final String actual = this.weatherForecast.getCityWeather(CITY_NAME, DATE_IN_RANGE);
-        assertEquals(PREDICTION_1.getWeatherStateName(), actual);
+        assertEquals(PREDICTION_FOR_DATE_IN_RANGE_1, actual);
     }
 
     @Test
     public void should_return_no_results_when_date_in_range_but_not_in_results() throws IOException {
+        given_has_prediction();
         given(this.clock.isDateBetweenRange(DATE_IN_RANGE_NOT_FOUND_TO_LOCAL_DATE, OFFSET_IN_DAYS)).willReturn(true);
-        final String forecastToday = weatherForecast.getCityWeather(CITY_NAME, DATE_IN_RANGE_NOT_FOUND);
-        assertEquals(forecastToday, NO_WEATHER_STATE);
+        final String actual = this.weatherForecast.getCityWeather(CITY_NAME, DATE_IN_RANGE_NOT_FOUND);
+        assertEquals(NO_WEATHER_STATE, actual);
     }
 }
